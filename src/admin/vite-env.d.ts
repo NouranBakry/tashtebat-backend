49afa5ed3584f8e79e2978456d91b6de3323486b
NouranBakry <nouranaymanbakry@gmail.com>
2025-08-28 13:08:14 +0300

    adding vendors route to admin dashboard

--- src/admin/vite-env.d.ts
+++ src/admin/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+/// <reference types="vite/client" />
+
+declare const __BASE__: "/app"
+declare const __BACKEND_URL__: "http://localhost:9000"
+declare const __STOREFRONT_URL__: "http://localhost:8000";
